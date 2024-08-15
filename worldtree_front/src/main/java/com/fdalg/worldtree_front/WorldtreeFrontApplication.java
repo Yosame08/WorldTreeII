f313a85dc3e89c7bc17403dcb5a3c459dc9056a6
Yosame <2503542239@qq.com>
2024-08-15 12:07:39 +0800

    create template

---
+++ worldtree_front/src/main/java/com/fdalg/worldtree_front/WorldtreeFrontApplication.java
@@ -0,0 +1,13 @@
+package com.fdalg.worldtree_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorldtreeFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorldtreeFrontApplication.class, args);
+    }
+
+}
---
+++ worldtree_front/src/test/java/com/fdalg/worldtree_front/WorldtreeFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdalg.worldtree_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorldtreeFrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
