d02bdd6ddfa6f56b4c829330993986e25d75f81c
Yosame <46925855+Yosame08@users.noreply.github.com>
2024-08-18 16:48:40 +0800

    Progress Bar & Rank Chart (#3)
    * Progress Bar Added
    
    * Rank Chart Added; demands modified

--- worldtree_front/src/services/infoService.js
+++ worldtree_front/src/services/infoService.js
@@ -1,6 +1,14 @@
 // src/services/infoService.js
 import axios from 'axios';
+import store from '@/services/loadService';
 
-export const fetchRankData = () => {
-    return axios.get('/api/rank');
+export const fetchRankData = async () => {
+    store.commit('load');
+    try {
+        return await axios.get('/api/rank');
+    } catch (error) {
+        throw error;
+    } finally {
+        store.commit('finish');
+    }
 };
