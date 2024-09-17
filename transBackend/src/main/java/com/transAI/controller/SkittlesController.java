fb06112130a83a3476bbbb4d64c513d893f95d6a
Tarzan1801 <100453626+Tarjan01@users.noreply.github.com>
2024-09-17 13:35:25 +0800

    api : skittles_init (#45)

--- transBackend/src/main/java/com/transAI/controller/SkittlesController.java
+++ transBackend/src/main/java/com/transAI/controller/SkittlesController.java
@@ -1,2 +1,26 @@
-package com.transAI.controller;public class SkittlesController {
+package com.transAI.controller;
+
+import com.transAI.pojo.Result;
+import com.transAI.service.SkittlesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/subtask/skittles")
+public class SkittlesController {
+
+    @Autowired
+    private SkittlesService skittlesService;
+
+    @PostMapping("/init")
+    public Result<List<Integer>> init(@RequestBody Map<String, Integer> params) {
+        Integer start = (Integer) params.get("start");
+        return Result.success(skittlesService.init(start));
+    }
 }
---
+++ transBackend/src/main/java/com/transAI/service/SkittlesService.java
@@ -0,0 +1,8 @@
+package com.transAI.service;
+
+import java.util.List;
+
+public interface SkittlesService {
+
+    List<Integer> init(int start);
+}
