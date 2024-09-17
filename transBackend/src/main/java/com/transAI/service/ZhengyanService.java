d96da653099af99ad893e26dbde79ea5bec8a946
Tarzan1801 <100453626+Tarjan01@users.noreply.github.com>
2024-09-17 14:00:53 +0800

    api : zhengyan fun (#47)

---
+++ transBackend/src/main/java/com/transAI/service/ZhengyanService.java
@@ -0,0 +1,10 @@
+package com.transAI.service;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public interface ZhengyanService {
+
+    int validate(List<Integer> list);
+}
