cc54d6bde6d1693830e5a560ef24666c205e1d8b
Tarzan1801 <100453626+Tarjan01@users.noreply.github.com>
2024-09-19 23:27:53 +0800

    ill api : wow_finish (#62)

---
+++ transBackend/src/main/java/com/transAI/controller/TartsController.java
@@ -0,0 +1,22 @@
+package com.transAI.controller;
+
+import com.transAI.pojo.Result;
+import com.transAI.service.TartsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/subtask/tarts")
+public class TartsController {
+
+    @Autowired
+    private TartsService tartsService;
+
+    @GetMapping("/w0w_f1n1sh3d")
+    public Result checkTarts() {
+        tartsService.add();
+        return Result.success();
+    }
+}
---
+++ transBackend/src/main/java/com/transAI/service/impl/TartsServiceImpl.java
@@ -0,0 +1,48 @@
+package com.transAI.service.impl;
+
+import com.transAI.mapper.TaskMapper;
+import com.transAI.mapper.TaskUserMapper;
+import com.transAI.mapper.UserTotalPointMapper;
+import com.transAI.pojo.TaskUser;
+import com.transAI.pojo.UserTotalPoint;
+import com.transAI.service.TartsService;
+import com.transAI.utils.ThreadLocalUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+@Transactional
+public class TartsServiceImpl implements TartsService {
+    @Autowired
+    private TaskUserMapper taskUserMapper;
+
+    @Autowired
+    private UserTotalPointMapper userTotalPointMapper;
+    @Override
+    public void add() {
+        Map<String, Object> map = ThreadLocalUtil.get();
+
+        int id = (int) map.get("id");
+        int taskId = 0;
+
+        TaskUser taskUser = taskUserMapper.getTaskUser(id, taskId);
+        if(taskUser == null) {
+            int score = 100;
+            taskUser = new TaskUser();
+            taskUser.setUserId(id);
+            taskUser.setTaskId(taskId);
+            taskUser.setStatus(1);
+            taskUser.setPoint(score);
+            taskUser.setTime(LocalDateTime.now());
+            taskUserMapper.insert(taskUser);
+            int pre_point = userTotalPointMapper.getMaxPoint(id);
+            // 输出pre_point
+
+            userTotalPointMapper.addPoint(id,pre_point + score);
+        }
+    }
+}
